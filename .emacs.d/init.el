fdf145c8d0725292af30af693f21a2dc84b4d31f
Gabrielle Guimarães <gabrielle1guim@gmail.com>
2021-12-22 08:35:37 -0300

    add other editors configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,7 +1,10 @@
 ;; Set up package.el to work with MELPA
 (require 'package)
+
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/") 
+
 (package-initialize)
 (package-refresh-contents)
 
@@ -12,3 +15,23 @@
 ;; Enable Evil
 (require 'evil)
 (evil-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#2e3436" "#a40000" "#4e9a06" "#c4a000" "#204a87" "#5c3566" "#729fcf" "#eeeeec"])
+ '(custom-enabled-themes '(tango))
+ '(custom-safe-themes
+   '("18bec4c258b4b4fb261671cf59197c1c3ba2a7a47cc776915c3e8db3334a0d25" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" default))
+ '(package-selected-packages '(dracula-theme evil)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
